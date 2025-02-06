298115e3139e5e5702a93646a12941e5f3537bd9
Lukas Fritze <12097997+LukasFritzeDev@users.noreply.github.com>
2025-02-06 20:00:01 +0100

    Add basice vite app with tanstack router

---
+++ packages/web2/vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import TanStackRouterVite from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+});
