20a2490eaf97e79929d0d5b0c04562af7387f5b0
Lukas Fritze <12097997+LukasFritzeDev@users.noreply.github.com>
2025-04-21 17:50:04 +0200

    Simplify error and not found components

--- packages/web2/app/components/errorRoot.tsx
+++ packages/web2/app/components/errorRoot.tsx
@@ -1,51 +1,13 @@
-import { ErrorRouteComponent, Link } from "@tanstack/react-router";
+import { ErrorRouteComponent } from "@tanstack/react-router";
 import { RootDocument } from "./rootDocument.tsx";
-import {
-  Breadcrumb,
-  CodeBlock,
-  ColumnLayout,
-  Content,
-  Heading,
-  InlineCode,
-  Label,
-  LabeledValue,
-  LayoutCard,
-  Section,
-} from "@mittwald/flow-react-components";
 
 export const ErrorRoot: ErrorRouteComponent = ({ error, info }) => {
   return (
     <RootDocument>
-      <ColumnLayout s={[1]}>
-        <Breadcrumb color="light">
-          <Link to="/">Projekt</Link>
-          <Link to="/">A11y Checker</Link>
-        </Breadcrumb>
-        <Heading level={1} color="light">
-          A11y Checker
-        </Heading>
-        <LayoutCard>
-          <Section>
-            <ColumnLayout>
-              <LabeledValue>
-                <Label>Info</Label>
-                <Content>{info?.toString()}</Content>
-              </LabeledValue>
-              <LabeledValue>
-                <Label>Error</Label>
-                <Content>{error.name}</Content>
-              </LabeledValue>
-              <LabeledValue>
-                <Label>Message</Label>
-                <Content>
-                  <InlineCode>{error.message}</InlineCode>
-                </Content>
-              </LabeledValue>
-            </ColumnLayout>
-            <CodeBlock code={error.stack ?? ""} copyable />
-          </Section>
-        </LayoutCard>
-      </ColumnLayout>
+      <p>Fehler</p>
+      <pre>{JSON.stringify(info, null, 2)}</pre>
+      <pre>{JSON.stringify(error, null, 2)}</pre>
+      {String(error)}
     </RootDocument>
   );
 };
--- packages/web2/app/components/notFoundRoot.tsx
+++ packages/web2/app/components/notFoundRoot.tsx
@@ -1,24 +1,11 @@
 import { NotFoundRouteComponent } from "@tanstack/react-router";
 import { RootDocument } from "./rootDocument.js";
-import {
-  Heading,
-  IconSearch,
-  IllustratedMessage,
-  LayoutCard,
-  Link,
-} from "@mittwald/flow-react-components";
 
 export const NotFoundRoot: NotFoundRouteComponent = ({ data }) => {
   console.error(data);
   return (
     <RootDocument>
-      <LayoutCard>
-        <IllustratedMessage>
-          <IconSearch />
-          <Heading>404 - Nicht gefunden</Heading>
-          <Link href="/">Zur Startseite</Link>
-        </IllustratedMessage>
-      </LayoutCard>
+      <p>404 – Nicht gefunden</p>
     </RootDocument>
   );
 };
