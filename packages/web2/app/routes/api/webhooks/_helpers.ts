f84b2a5836304b25cabe1cc9a712da78081016cb
Lukas Fritze <12097997+LukasFritzeDev@users.noreply.github.com>
2025-05-04 13:40:14 +0200

    Fix assertContextType

--- packages/web2/app/routes/api/webhooks/_helpers.ts
+++ packages/web2/app/routes/api/webhooks/_helpers.ts
@@ -1,7 +1,7 @@
 import { json } from "@tanstack/react-start";
 
 export const assertContextType = (context: { kind: string }) => {
-  if (context.kind !== "project") {
+  if (context.kind !== "project" && context.kind !== "customer") {
     throw json(
       { message: `Wrong context type '${context.kind}'` },
       { status: 400 },
