e420b1c81a62dc4c24c81862064843d756f8c906
Ihor Rudenko <i.rudenko@mittwald.de>
2025-09-23 17:28:26 +0200

    Fix ContextualHelp in ‘Nächste Ausführung’

--- packages/web2/app/components/profile/tabs/settings.tsx
+++ packages/web2/app/components/profile/tabs/settings.tsx
@@ -2,15 +2,15 @@ import { GeneralSettings } from "./settings/general.tsx";
 import { IntarvallSettings } from "./settings/intervall.tsx";
 import { PathSettings } from "./settings/paths.tsx";
 import { CurrentScan } from "../currentScan.tsx";
-import { Flex } from "@mittwald/flow-remote-react-components";
+import {  Section } from "@mittwald/flow-remote-react-components";
 
 export const Settings = () => {
   return (
-    <Flex direction="column">
+    <Section>
       <CurrentScan />
       <GeneralSettings />
       <IntarvallSettings />
       <PathSettings />
-    </Flex>
+    </Section>
   );
 };
