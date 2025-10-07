e736783d98d02bdf1db4ad19987a8683652f76ed
Dario Eder <d.eder@mittwald.de>
2025-10-07 08:41:36 +0200

    define root in vite.config.ts to make it work properly in a monorepo

--- packages/web2/vite.config.ts
+++ packages/web2/vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: __dirname,
   build: {
     minify: false,
   },
